f8db30087cfe2335dd4e627e23b71ed198473f4f
YWT <theabc50111@gmail.com>
2025-04-13 18:14:34 +0000

    [style] rename files for practice of students

--- ch09/cors(ans).py
+++ ch09/cors(ans).py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # ----------practice start------------
 @app.route('/')
 def index():
-    return render_template('cors(ans).html',
+    return render_template('cors.html',
                            api_url_origin_allow_cors='http://192.168.0.17:5050/',
                            api_url_origin_not_allow_cors='http://192.168.0.17:5051/',  # press F12 to open dev tool, and check the console
                            page_header='Cross-Origin Resource Sharing (CORS) - Allow and Not Allow',)
