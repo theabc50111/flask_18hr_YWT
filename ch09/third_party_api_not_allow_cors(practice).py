d39f3629278e8c1acda63f5d1c33ed5aec3e60a1
YWT <theabc50111@gmail.com>
2025-04-13 18:26:53 +0000

    [feat & style] add ch09/threaded.py, ch09/cors.py, revise ch07/file name

---
+++ ch09/third_party_api_not_allow_cors(practice).py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# ----------practice start------------
+# ----------practice end--------------
+
+
+if __name__ == '__main__':
+    # ----------practice start------------
+    # ----------practice end--------------
