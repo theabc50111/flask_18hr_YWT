bc130924160114a358adac511502e6c73b94f49f
YWT <theabc50111@gmail.com>
2025-06-16 16:00:08 +0000

    [feat] add ch18 for practice

---
+++ ch18/cache_cached(practice).py
@@ -0,0 +1,18 @@
+import time
+
+from flask import Flask
+# ========== practice start ==========
+# ========== practice end ==========
+
+app = Flask(__name__)
+
+# ========== practice start ==========
+# ========== practice end ==========
+
+
+# ========== practice start ==========
+# ========== practice end ==========
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
---
+++ ch18/cache_momoize(practice).py
@@ -0,0 +1,33 @@
+import time
+
+from flask import Flask, request
+from flask_caching import Cache
+
+app = Flask(__name__)
+
+app.config['CACHE_TYPE'] = 'SimpleCache'
+cache = Cache(app)
+
+
+# ========== practice start ==========
+@app.route('/get_computation')
+def get_computation():
+    result = slow_compute(request.args.get('n', default=1, type=int))
+    return f"<h1>result of slow_compute() = {result}</h1>"
+# ========== practice end ==========
+
+
+# ========== practice start ==========
+@cache.memoize(timeout=180)
+def slow_compute(n):
+    # the cache key is based on the function name and its arguments
+    # so if you call slow_compute(1) and then slow_compute(2), the results will be cached separately
+    for i in range(10, 0, -1):
+        time.sleep(1)
+        print(f"Computing: {i} seconds remaining")
+    return n * 10
+# ========== practice end ==========
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
