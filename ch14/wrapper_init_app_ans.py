2d302c951fc61648e677d79669a1a0374631a141
YWT <theabc50111@gmail.com>
2025-06-28 15:23:48 +0000

    [feat] add practice part

--- ch14/wrapper_init_app_ans.py
+++ ch14/wrapper_init_app_ans.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
-
 # ========== practice start ==========
 from wrapper_init_auth import init_auth
+
 # ========== practice end ==========
 
 app = Flask(__name__)
@@ -11,8 +11,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", page_header="page_header")
 
-
+# ========== practice start ==========
 init_auth(app)
+# ========== practice end ==========
 
 if __name__ == "__main__":
     print(app.url_map)  # To check the route `/login`, you can either watch it on a terminal or connect to /login through a browser
--- ch14/wrapper_init_app_practice.py
+++ ch14/wrapper_init_app_practice.py
@@ -11,7 +11,8 @@ def index():
     return render_template("index.html", page_header="page_header")
 
 
-init_auth(app)
+# ========== practice start ==========
+# ========== practice end ==========
 
 if __name__ == "__main__":
     print(app.url_map)  # To check the route `/login`, you can either watch it on a terminal or connect to /login through a browser
